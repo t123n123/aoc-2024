2bb81945fb8989dc6bd361b43dc10061b9563ec3
Mihai Spinei <spineim22@gmail.com>
2024-12-01 11:35:58 +0100

    day 1 cleaner

--- 1day.rkt
+++ 1day.rkt
@@ -4,25 +4,26 @@
 (define file (open-input-file "./1day.input"))
 
 (define n 1000)
-(define a (list))
-(define b (list))
-
-(for ([_ n])
-  (set! a (cons (read file) a))
-  (set! b (cons (read file) b))
-  )
+(define ab
+  (for/list ([_ n])
+    (cons (read file) (read file))))
+(define a (map car ab))
+(define b (map cdr ab))
 
 ;; Part 1
 
 (set! a (sort a <))
 (set! b (sort b <))
+(set! ab (map cons a b))
 
-(apply + (for/list ([i (length a)])
-           (abs (- (list-ref a i) (list-ref b i)))))
+(apply +
+       (for/list ([i ab])
+         (abs (- (car i) (cdr i)))))
 
 ;; Part 2
 
 (define ha (list->set a))
 
-(apply + (for/list ([i (length a)])
-           (if (set-member? ha (list-ref b i)) (list-ref b i) 0)))
+(apply +
+       (for/list ([i b])
+         (if (set-member? ha i) i 0)))
